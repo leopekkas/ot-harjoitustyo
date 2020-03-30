a6f0fe81d2b3cfaccc9ed9286ef77e645a0fa327
Leopekka Saraste <leopekkas@gmail.com>
2020-03-30 20:17:38 +0300

    Pientä lisäilyä toiminnallisuuksiin

--- Aurinkokuntasimulaattori/src/main/java/aurinkokuntasimulaattori/Main.java
+++ Aurinkokuntasimulaattori/src/main/java/aurinkokuntasimulaattori/Main.java
@@ -6,26 +6,44 @@ import javafx.scene.Scene;
 import javafx.stage.Stage; 
 import javafx.scene.shape.Circle; 
 
+
+import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.geometry.Bounds;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+import javafx.util.Duration;
+
+
 public class Main extends Application {
     
     Kappale keskusta;
     
+    // Testailen aluksi vaan javafx animation toimintoja ennen minkään
+    // käytännöllisen toiminnallisuuden lisäilemistä
+    
     @Override
     public void start(Stage stage) {
+        Pane canvas = new Pane();
+        
+        Scene scene = new Scene(canvas, 500, 300);
+        
         // Luodaan ikkunaan ympyrä
         Circle circle = new Circle();
         
+        canvas.getChildren().add(circle);
+        
         // Lisätään keskuskappale
-        Kappale keskusta = new Kappale("Aurinko", 100000, 100);
+        Kappale keskusta = new Kappale("Aurinko", 100000, 10);
         
         // Merkataan ympyrän ominaisuudet
-        circle.setCenterX(300.0f);
-        circle.setCenterY(135.0f);
+        circle.setCenterX(100.0f);
+        circle.setCenterY(35.0f);
         circle.setRadius(keskusta.getRadius());
-        
-        Group root = new Group(circle);
-        
-        Scene scene = new Scene(root, 900, 700);
         
         //Lisätään ikkunalle otsikko
         stage.setTitle("Ympyrän muodostus");
@@ -33,6 +51,14 @@ public class Main extends Application {
         stage.setScene(scene);
         
         stage.show();
+        
+        Bounds reunat = canvas.getBoundsInLocal();
+        
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(3)
+                , new KeyValue(circle.layoutXProperty(), reunat.getMaxX()-circle.getRadius())));
+        timeline.setCycleCount(2);
+        timeline.play();
+
     }
     
     public static void main(String[] args) {
